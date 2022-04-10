40928458ca1b9dc404030f055b5a39eb242c8e2e
kellsnc <kellspam@outlook.fr>
2022-04-11 00:49:27 +0200

    Cheese Attack behaviour

--- CreamSA1/cheese.cpp
+++ CreamSA1/cheese.cpp
@@ -11,7 +11,8 @@
 enum : __int8
 {
 	MODE_INIT,
-	MODE_NORMAL
+	MODE_NORMAL,
+	MODE_ATTACK
 };
 
 enum : __int8
@@ -107,6 +108,74 @@ static void __cdecl CheeseDisplay(task* tp)
 	}
 }
 
+static void CheeseNormal(taskwk* twp, taskwk* ptwp, playerwk* ppwp)
+{
+	if (ptwp->mode == 20i8)
+	{
+		twp->mode = MODE_ATTACK;
+		return;
+	}
+
+	// Offset from player
+	NJS_POINT3 v = { -1.5f + (0.25f * njCos(FrameCounterUnpaused * 80)), ppwp->p.height - 1.0f + (0.8f * njSin(FrameCounterUnpaused * 80)), 2.3f + (1.0f * njCos(FrameCounterUnpaused * 180)) };
+
+	// Calc world position
+	njPushMatrix(_nj_unit_matrix_);
+	njTranslateEx(&ptwp->pos);
+	njRotateZ_(ptwp->ang.z);
+	njRotateX_(ptwp->ang.x);
+	njRotateY_(-LOWORD(ptwp->ang.y));
+	njCalcPoint(0, &v, &v);
+	njPopMatrixEx();
+
+	twp->pos = GetPosition(&twp->pos, &v, 0.1f); // Move to target
+	twp->ang.y = AdjustAngle(twp->ang.y, ptwp->ang.y, 0x300); // Rotate to player angle
+
+	// Choose animation based on distance from target
+	NJS_VECTOR pos = twp->pos;
+	njSubVector(&pos, &v);
+	if (njScalor(&pos) > 1.0f)
+	{
+		twp->smode = ANIM_FLY;
+	}
+	else
+	{
+		twp->smode = ANIM_IDLE;
+	}
+}
+
+static void CheeseAttack(taskwk* twp, taskwk* ptwp, playerwk* ppwp)
+{
+	if (ptwp->mode != 20i8)
+	{
+		twp->mode = MODE_NORMAL;
+		twp->timer.l = 0;
+		return;
+	}
+
+	Angle attack_ang = -(++twp->timer.l * 2000);
+
+	// Get position around circle
+	NJS_POINT3 v;
+	v.x = njCos(attack_ang) * 10.0f;
+	v.y = ppwp->p.height * 0.5f;
+	v.z = njSin(attack_ang) * 10.0f;
+
+	// Calc world position
+	njPushMatrix(_nj_unit_matrix_);
+	njTranslateEx(&ptwp->pos);
+	njRotateZ_(ptwp->ang.z);
+	njRotateX_(ptwp->ang.x);
+	njRotateY_(-LOWORD(ptwp->ang.y));
+	njCalcPoint(0, &v, &v);
+	njPopMatrixEx();
+
+	twp->pos = GetPosition(&twp->pos, &v, 0.33f); // Move to target
+	twp->ang.y = attack_ang + 0xC000;
+
+	twp->smode = ANIM_ATTACK;
+}
+
 static void __cdecl CheeseExec(task* tp)
 {
 	auto twp = tp->twp;
@@ -130,33 +199,10 @@ static void __cdecl CheeseExec(task* tp)
 		twp->mode = MODE_NORMAL;
 		break;
 	case MODE_NORMAL:
-		// Offset from player
-		NJS_POINT3 v = { -1.5f + (0.25f * njCos(FrameCounterUnpaused * 80)), ppwp->p.height - 1.0f + (0.8f * njSin(FrameCounterUnpaused * 80)), 2.3f + (1.0f * njCos(FrameCounterUnpaused * 180)) };
-
-		// Calc world position
-		njPushMatrix(_nj_unit_matrix_);
-		njTranslateEx(&ptwp->pos);
-		njRotateZ_(ptwp->ang.z);
-		njRotateX_(ptwp->ang.x);
-		njRotateY_(-LOWORD(ptwp->ang.y));
-		njCalcPoint(0, &v, &v);
-		njPopMatrixEx();
-
-		twp->pos = GetPosition(&twp->pos, &v, 0.1f); // Move to target
-		twp->ang.y = AdjustAngle(twp->ang.y, ptwp->ang.y, 0x300); // Rotate to player angle
-
-		// Choose animation based on distance from target
-		NJS_VECTOR pos = twp->pos;
-		njSubVector(&pos, &v);
-		if (njScalor(&pos) > 1.0f)
-		{
-			twp->smode = ANIM_FLY;
-		}
-		else
-		{
-			twp->smode = ANIM_IDLE;
-		}
-
+		CheeseNormal(twp, ptwp, ppwp);
+		break;
+	case MODE_ATTACK:
+		CheeseAttack(twp, ptwp, ppwp);
 		break;
 	}
 
