c78f8c854bcf4c27adafba1871f33a3471737cf1
kellsnc <kellspam@outlook.fr>
2022-04-11 04:04:52 +0200

    Change some sounds

--- CreamSA1/mod.cpp
+++ CreamSA1/mod.cpp
@@ -16,6 +16,9 @@ extern "C"
 		HookCHRMODELS();
 		InitEars();
 		InitCheese();
+
+		WriteData((int*)0x45D74B, 774); // Different sound for attack
+		WriteData((int*)0x45C6E9, 1252); // Different voice for dash hoops
 	}
 
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
